912449174668487041a8221b41cfba005cdc8da6
grimaud <m-grimaud@users.noreply.github.com>
2021-09-29 19:56:16 -0500

    Clean up event listener on font resize.

--- src/components/SlideDiv.tsx
+++ src/components/SlideDiv.tsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useRef, useState } from 'react';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 
 export interface SlideDivProps {
   slideContents: Array<String>
@@ -8,27 +8,33 @@ export default function SlideDiv(props: SlideDivProps) {
   const [fontSize, setFontSize] = useState(200);
   const rectRef = useRef<HTMLParagraphElement>(null);
 
+  const resizeFont = () => {
+    const elementRef = rectRef?.current?.getBoundingClientRect();
+    const parentRect = rectRef?.current?.parentElement?.getBoundingClientRect();
+    if (elementRef !== undefined && parentRect !== undefined) {
+      const k = elementRef.width / fontSize;
+      const widthFontSize = parentRect.width / k;
+      const l = elementRef.height / fontSize;
+      const heightFontSize = parentRect.height / l;
+      if (heightFontSize !== Infinity) {
+        setFontSize(Math.max(Math.min(heightFontSize, widthFontSize), 200));
+      }
+    }
+    else {
+      setFontSize(200);
+    }
+  };
+
   useLayoutEffect(() => {
-    const resizeFont = () => {
-      const elementRef = rectRef?.current?.getBoundingClientRect();
-      const parentRect = rectRef?.current?.parentElement?.getBoundingClientRect();
-      if (elementRef !== undefined && parentRect !== undefined) {
-        const k = elementRef.width / fontSize;
-        const widthFontSize = parentRect.width / k;
-        const l = elementRef.height / fontSize;
-        const heightFontSize = parentRect.height / l;
-        if (heightFontSize !== Infinity) {
-          setFontSize(Math.max(Math.min(heightFontSize, widthFontSize), 200));
-        }
-      }
-      else {
-        setFontSize(200);
-      }
-    };
     resizeFont();
-    window.addEventListener("resize", resizeFont);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeFont);
+
+    return () => window.removeEventListener("resize", resizeFont);
+  });
 
   return (
     <>
